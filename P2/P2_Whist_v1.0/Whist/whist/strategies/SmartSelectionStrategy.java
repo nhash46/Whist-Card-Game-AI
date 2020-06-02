e250ad9e71c7552a419022c6156f884e55ddda94
Iasona1 <62829720+Iasona1@users.noreply.github.com>
2020-06-02 18:54:05 +1000

    updating smart selection

--- P2/P2_Whist_v1.0/Whist/whist/strategies/SmartSelectionStrategy.java
+++ P2/P2_Whist_v1.0/Whist/whist/strategies/SmartSelectionStrategy.java
@@ -26,39 +26,54 @@ public class SmartSelectionStrategy implements ICardSelectionStrategy{
 			System.out.println(properties.getCardsPlayed()[i].toString());
 		}
 		
-		ArrayList<Card> cardList = new ArrayList<Card>();
+		ArrayList<Card> legalCards = new ArrayList<Card>();
 		// Get a list of legal cards
 		if( lead == null ) {
-			cardList = hand.getCardList();
+			legalCards = hand.getCardList();
 		}
 		else{
 			for(int i = 0; i < hand.getNumberOfCards(); i++) {
 				if( (hand.get(i).getSuit().toString().equals(lead.toString())) || 
 						(hand.get(i).getSuit().toString().equals(trump.toString())) ) {
-					cardList.add(hand.get(i));
+					legalCards.add(hand.get(i));
 				}
 			}
 		}
-			
-			
 		
-			// If can win, play a winner
+		// Need to sort list
 		
-			// If cannot win, play lowest legal card
-			
-			
-		//If no legal cards, play card with lowest rank
+		if( legalCards.isEmpty() ) {
+			//If no legal cards, play card with lowest rank
+			//selected = selectLowest(hand);
+			//for now play any random card
+		}
+		else {
+				// If we have a lead suit
+				if( hasSuit(hand, lead) == true ) {
+					// If maxLead() is a winner, play it
+					// If maxLead() is not a winner, play minLead()
+				}
+				// else we have we have a trump suit
+				else {
+					// If minTrump() is a winner, play it
+					// If minTrump() is not a winner, play minTrump()
+				}
+		}
 		
-		
-		
-		
-		selected = selectLowest(hand);
+		selected = 1;
 		return hand.get(selected);
 	}
 	
 	
 	
-	public int hasSuit(Hand hand, Suit suit) {
+	public int selectLowest(Hand hand) {
+		
+		return 0;
+	}
+	
+	
+	
+	public boolean hasSuit(Hand hand, Suit suit) {
 
 		int num_cards = hand.getNumberOfCards();
 		String suitString = suit.toString();
@@ -66,17 +81,17 @@ public class SmartSelectionStrategy implements ICardSelectionStrategy{
 		for(int i = 0; i < num_cards; i++) {
 			String currSuitString = hand.get(i).getSuit().toString();
 			if( currSuitString.equals(suitString) ) {
-				return i;
+				return true;
 			}
 		}
-		return -1;
+		return false;
 	}
 	
 	
-	
-	public int selectLowest(Hand hand) {
+	public Card findWinner() {
 		
-		return ;
+		
+		
 	}
 	  
 	  
